99afda18c96c8561284c90eb1d212b7031bb59d1
Sergi Rojas <sergi.rojas@soprasteria.com>
2021-12-20 09:24:09 +0100

    refactor

--- MarvelHeroes/Commons/Constants.swift
+++ MarvelHeroes/Commons/Constants.swift
@@ -11,4 +11,18 @@ public enum Constants {
     static let urlBase = "https://gateway.marvel.com/v1/public/characters"
     static let publicKey = "aa8d3daa1e6d77ea7a0905cef180201f"
     static let privateKey = "302dfcf452df511a6e481ce1950c8f1260f056cd"
+    
+}
+
+public enum Params {
+    static let apiKey = "apikey"
+    static let ts = "ts"
+    static let hash = "hash"
+    static let limitString = "limit"
+    static let offsetString = "offset"
+}
+
+enum ServiceError: Error {
+    case responseError
+    case mappingError
 }
--- MarvelHeroes/Data/CharactersRepository.swift
+++ MarvelHeroes/Data/CharactersRepository.swift
@@ -9,53 +9,39 @@ import Foundation
 import Alamofire
 import RxSwift
 
-final class DefaultCharactersRepository {
-    
-}
-
-extension DefaultCharactersRepository: CharactersRepository {
+final class DefaultCharactersRepository: CharactersRepository {
     
     private enum RepoConstants {
-        static let apiKey = "apikey"
-        static let ts = "ts"
-        static let hash = "hash"
-        static let limitString = "limit"
         static let limit = 20
-        static let offsetString = "offset"
     }
     
     func charactersList(skip: Int? = 0) -> Single<CharactersListResponse> {
         return Single.create { single -> Disposable in
             let timeStamp = Utils.getTimeStamp()
             var parameters: [String : Any] = [
-                RepoConstants.apiKey : Constants.publicKey,
-                RepoConstants.ts : timeStamp,
-                RepoConstants.hash : Utils.getHash(timeStamp),
-                RepoConstants.limitString : RepoConstants.limit
+                Params.apiKey : Constants.publicKey,
+                Params.ts : timeStamp,
+                Params.hash : Utils.getHash(timeStamp),
+                Params.limitString : RepoConstants.limit
             ]
             
             if let skip = skip {
-                parameters[RepoConstants.offsetString] = skip
+                parameters[Params.offsetString] = skip
             }
             
             AF.request(Constants.urlBase, parameters: parameters).response { response in
                 switch response.data {
                 case .some(let data):
                     guard let response = try? JSONDecoder().decode(CharactersListResponse.self, from: data) else {
-                        single(.failure(RepositoryError.mappingError))
+                        single(.failure(ServiceError.mappingError))
                         return
                     }
                     single(.success(response))
                 default:
-                    single(.failure(RepositoryError.responseError))
+                    single(.failure(ServiceError.responseError))
                 }
             }
             return Disposables.create()
         }
     }
-}
-
-enum RepositoryError: Error {
-    case responseError
-    case mappingError
 }
